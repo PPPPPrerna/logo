4ac515e482ec236fd51139e3435b6701a96fbc7a
PPPPPrerna <pandeyprerna248@gmail.com>
2020-09-19 12:18:49 +0530

    mess

---
+++ src/test/java/quick/Arr/Ar.java
@@ -0,0 +1,66 @@
+package quick.Arr;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Ar {
+
+	@Test
+	public void lisa() {
+		
+Integer a[] = {2,3,7,4,5};
+		
+//{6,2,4,7,3}
+		int x=9;
+		
+	
+		
+		ArrayList<Integer> al = new ArrayList<Integer>( Arrays.asList(a));
+		
+		System.out.println(al);
+		
+		//for(int i=0;i<al.size();i++) {
+			
+			//for(int j=i+1;j<al.size();j++) {
+			
+			//int temp ;
+			//temp=x-al.get(i);
+			
+			//if(al.get(i)+al.get(j)==x) {
+				
+				//System..println("value is"+al.get(i) + "and" + al.get(j));
+		//	}
+			
+			//}		
+				
+				
+				}
+}
+
+	
+			
+
+			
+			
+			
+			
+			
+			
+		
+	
+		
+		
+
+	
+
